da8e0a8db78b4654b08ec362f836717f2b47e690
arthurdm10 <arthurdemacq@hotmail.com>
2019-05-31 07:46:52 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MainPage());
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
