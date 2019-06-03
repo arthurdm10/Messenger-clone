d76af7c0ca985cb9af59e384c6ea8d5d104f806b
arthurdm10 <arthurdemacq@hotmail.com>
2019-06-03 13:35:40 -0300

    Updates

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+<img src="/main.jpeg"  width=200> <img src="chat.jpeg"  width=200>
--- lib/chat.dart
+++ lib/chat.dart
@@ -3,6 +3,8 @@ import 'package:messenger_clone/widgets/appBar_action_icon.dart';
 import 'package:messenger_clone/widgets/msg_status_icons.dart';
 import 'package:messenger_clone/widgets/online_avatar.dart';
 
+import 'widgets/badge.dart';
+
 class Chat extends StatelessWidget {
   final String chatTitle;
   const Chat({this.chatTitle});
@@ -15,21 +17,66 @@ class Chat extends StatelessWidget {
         child: ListView(
           padding: EdgeInsets.only(top: 5),
           children: <Widget>[
-            msgContainer("PKDSAKOPAdkpopakOSK PDOASK PODAS", true),
-            msgContainer("PKDSAKOPAdkpopakOSK PDOASK PODAS", false),
-            msgContainer("PKDSAKOPAdkpopakOSK PDOASK PODAS", true),
-            msgContainer("PKDSAKOPAdkpopakOSK PDOASK PODAS", false),
-            msgContainer("PKDSAKOPAdkpopakOSK PDOASK PODAS", false),
-            msgContainer("PKDSAKOPAdkpopakOSK PDOASK PODAS", false),
-            msgContainer("PKDSAKOPAdkpopakOSK PDOASK PODAS", true),
-            msgContainer("PKDSAKOPAdkpopakOSK PDOASK PODAS", false),
+            msgContainer(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                true),
+            msgContainer(
+                "Sit amet facilisis magna etiam tempor. Cursus metus aliquam eleifend mi in nulla",
+                false),
+            msgContainer(
+                "Ac ut consequat semper viverra nam libero justo laoreet sit. Malesuada fames ac turpis egestas ",
+                true),
+            msgContainer(
+                "Ac ut consequat semper viverra nam libero justo laoreet sit. Malesuada fames Consectetur adipiscing elit ut aliquam. Fringilla urna porttitor rhoncus dolor purus non",
+                false),
+            msgContainer(
+                "Ultrices tincidunt arcu non sodales neque. Viverra accumsan in nisl nisi scelerisque. Consectetur adipiscing elit ut aliquam. Fringilla urna porttitor rhoncus dolor purus non",
+                false,
+                showAvatar: true),
+            msgContainer(
+                "Quis viverra nibh cras pulvinar mattis nunc. Ultricies mi eget mauris",
+                true),
           ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 55,
+          // color: Colors.red,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              ...bottomIcons(),
+              Container(
+                width: 160,
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(left: 15),
+                    hintText: "Aa",
+                    filled: true,
+                    fillColor: Color(0xffefefef),
+                    suffixIcon: Icon(
+                      Icons.sentiment_satisfied,
+                      color: Color(0xff0f84fd),
+                    ),
+                    // fillColor: Colors.red,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.all(Radius.circular(50)),
+                    ),
+                  ),
+                ),
+              ),
+              Icon(Icons.thumb_up, size: 28, color: Color(0xff0f84fd))
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Row msgContainer(String msg, bool sentByUser) {
+  Row msgContainer(String msg, bool sentByUser, {bool showAvatar = false}) {
     Map msgConfig = {
       true: {
         "backgroundColor": Color(0xff0f84fd),
@@ -39,41 +86,76 @@ class Chat extends StatelessWidget {
             padding: EdgeInsets.all(2),
             child: MsgStatusIcon.msgReceived(size: 14)),
         "containerBorder": BorderRadius.only(
-          topLeft: Radius.circular(15),
-          bottomLeft: Radius.circular(15),
-          topRight: Radius.circular(8),
-          bottomRight: Radius.circular(8),
+          topLeft: Radius.circular(20),
+          bottomLeft: Radius.circular(20),
+          topRight: Radius.circular(5),
+          bottomRight: Radius.circular(5),
         ),
+        "msgAddReaction": Container(),
+        "userAvatar": Container(),
       },
       false: {
-        "backgroudColor": Color(0xffefefef),
+        "backgroundColor": Color(0xffefefef),
+        // "backgroundColor": Colors.red,
         "textColor": Colors.black,
         "rowAlignment": MainAxisAlignment.start,
         "msgStatusIcon": Container(),
         "containerBorder": BorderRadius.only(
-          topLeft: Radius.circular(8),
-          bottomLeft: Radius.circular(8),
-          topRight: Radius.circular(15),
-          bottomRight: Radius.circular(15),
+          topLeft: Radius.circular(5),
+          bottomLeft: Radius.circular(5),
+          topRight: Radius.circular(20),
+          bottomRight: Radius.circular(20),
+        ),
+        "msgAddReaction": Row(
+          children: <Widget>[
+            Icon(Icons.mood, size: 14, color: Color(0xffbfc3c8)),
+            Icon(Icons.add, size: 14, color: Color(0xffbfc3c8)),
+          ],
         ),
       }
     };
+
+    if (showAvatar && sentByUser == false) {
+      msgConfig[false]["userAvatar"] = Padding(
+        padding: EdgeInsets.only(left: 10, right: 6),
+        child: onlineAvatar(16,
+            badgeAlignFactor: Size(2.7, 3),
+            avatarAlignFactor: Size(1, 3.3),
+            badgePadding: 2.5,
+            borderPadding: 1.5),
+      );
+    } else {
+      msgConfig[false]["userAvatar"] = Container(
+        padding: EdgeInsets.only(left: 42, right: 6),
+      );
+    }
+
     var config = msgConfig[sentByUser];
 
     return Row(
       mainAxisAlignment: config["rowAlignment"],
       crossAxisAlignment: CrossAxisAlignment.end,
       children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(9),
-          constraints: BoxConstraints(maxWidth: 180),
-          decoration: BoxDecoration(
-              color: config["backgroundColor"],
-              borderRadius: config["containerBorder"]),
-          child: Text(
-            msg,
-            style: TextStyle(color: config["textColor"]),
-          ),
+        config["userAvatar"],
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(9),
+              constraints: BoxConstraints(maxWidth: 180),
+              decoration: BoxDecoration(
+                  color: config["backgroundColor"],
+                  borderRadius: config["containerBorder"]),
+              child: Text(
+                msg,
+                style: TextStyle(color: config["textColor"]),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 10),
+              child: config["msgAddReaction"],
+            )
+          ],
         ),
         config["msgStatusIcon"],
       ],
@@ -153,5 +235,15 @@ class Chat extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  List bottomIcons() {
+    const double iconSize = 27;
+    return [
+      Icon(Icons.more_horiz, size: iconSize, color: Color(0xff0f84fd)),
+      Icon(Icons.photo_camera, size: iconSize, color: Color(0xff0f84fd)),
+      Icon(Icons.photo, size: iconSize, color: Color(0xff0f84fd)),
+      Icon(Icons.mic, size: iconSize, color: Color(0xff0f84fd)),
+    ];
   }
 }
